626f57ae62867e0397c918a11f75b0000cc943d7
OnlineFlightBooking <114073437+OnlineFlightBooking@users.noreply.github.com>
2022-09-28 22:48:30 +0530

    Add files via upload

---
+++ CustomersMasterController.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FLightBookingSystem.DatabaseLayer;
+using FLightBookingSystem.Models;
+
+namespace FLightBookingSystem.Controllers.Customer
+{
+    public class CustomersMasterController : Controller
+    {
+        private FlightBookingEntities db = new FlightBookingEntities();
+
+        // GET: CustomersMaster
+        public ActionResult Index()
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login","Login");
+            }
+            var lst = db.Customers.Where(x => x.ID == iduser).ToList();
+            return View(lst);
+        }
+
+        // GET: CustomersMaster/Details/5
+        public ActionResult Details(int? id)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FLightBookingSystem.DatabaseLayer.Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // GET: CustomersMaster/Create
+        public ActionResult Create()
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var cust1 = new DatabaseLayer.Customer();
+            return View(cust1);
+        }
+
+        // POST: CustomersMaster/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,EmailAddress,MobileNo,Password,IsAdmin,DOB,CIty,AdharCard,Gender")] FLightBookingSystem.DatabaseLayer.Customer customer)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(customer);
+        }
+
+        // GET: CustomersMaster/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            if (userInCookie == null)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FLightBookingSystem.DatabaseLayer.Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: CustomersMaster/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,EmailAddress,MobileNo,Password,IsAdmin,DOB,CIty,AdharCard,Gender")] DatabaseLayer.Customer customer)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+
+        // GET: CustomersMaster/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            FLightBookingSystem.DatabaseLayer.Customer customer = db.Customers.Find(id);
+            if (customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        // POST: CustomersMaster/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            FLightBookingSystem.DatabaseLayer.Customer customer = db.Customers.Find(id);
+            db.Customers.Remove(customer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Book()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDetails()
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+          
+            var list = db.Bookings.Where(x => x.CustomerID == iduser);
+            return View("ViewDetails", list);
+        }
+
+        public ActionResult CancelBooking()
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var list = db.Bookings.Where(x => x.CustomerID == iduser);
+            ViewBag.BookingID = new SelectList(list, "BookingID", "BookingID");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CancelBooking(BookingModel booking)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+           
+            var first = db.Bookings.Where(x => x.CustomerID == iduser);
+            if (first != null)
+            {
+                foreach (var item in first)
+                {
+                    item.Status = "Canceld";
+                }
+                // first.BookingDate = booking.SelectedDate;
+
+                db.SaveChanges();
+                ViewBag.Message = "your Flight Canceled Payment Refunded : " + first.FirstOrDefault().Price;
+
+                var list = db.Bookings.Where(x => x.CustomerID == iduser);
+                ViewBag.BookingID = new SelectList(list, "BookingID", "BookingID");
+
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "Please select Proper Booking Id and Date";
+                var list = db.Bookings.Where(x => x.CustomerID == iduser);
+                ViewBag.BookingID = new SelectList(list, "BookingID", "BookingID");
+                return View();
+            }
+        }
+
+        public ActionResult ResheduleBooking()
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+          
+            var list = db.Bookings.Where(x => x.CustomerID == iduser);
+            ViewBag.BookingID = new SelectList(list, "BookingID", "BookingID");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ResheduleBooking(BookingModel booking)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            var first = db.Bookings.Where(x => x.CustomerID == iduser).ToList();
+            if (first != null)
+            {
+                foreach (var item in first)
+                {
+                    item.BookingDate = DateTime.Now;
+                    item.Status = "Resheduled";
+                }
+
+                db.SaveChanges();
+                ViewBag.Message = "your Flight Resheduled";
+                var list = db.Bookings.Where(x => x.CustomerID == iduser && x.BookingDate == booking.SelectedDate);
+                ViewBag.BookingID = new SelectList(list, "BookingID", "BookingID");
+                return View();
+            }
+            else
+            {
+                ViewBag.Message = "Please select Proper Booking Id and Date";
+                var list = db.Bookings.Where(x => x.CustomerID == iduser);
+                ViewBag.BookingID = new SelectList(list, "BookingID", "BookingID");
+                return View();
+            }
+        }
+
+        public ActionResult Confrim(int id)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            var flight = db.Flights.FirstOrDefault(x => x.FlightID == id);
+            db.Bookings.Add(new Booking
+            {
+                CustomerID = iduser,
+                FlightID = flight.FlightID,
+                BookingDate = flight.DeptTime,
+                FromCIty = flight.FromCity,
+                ToCity = flight.ToCity,
+                PaymentStatus = "Done",
+                Price = flight.Price,
+                Status = "Confirm",
+                TotalSeat = 1,
+            });
+            db.SaveChanges();
+            return View("Confrim");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Book(FlightSearch flightSearch)
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            var flights = db.Flights.Where(x => x.FromCity == flightSearch.FromCity && x.ToCity == flightSearch.ToCity && x.DeptTime >= flightSearch.StartTime);
+            return View("SearchResult", flights);
+        }
+        public ActionResult SearchResult(List<Flight> flight)
+        {
+
+            var userInCookie = Request.Cookies["UserInfo"];
+            int iduser = Convert.ToInt32(userInCookie["idUser"]);
+            if (iduser == 0)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+            return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LoginController.cs
@@ -0,0 +1,170 @@
+﻿using FLightBookingSystem.DatabaseLayer;
+using System;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Net.Mail;
+using System.Collections.Generic;
+
+namespace FLightBookingSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        private FlightBookingEntities db = new FlightBookingEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Logout()
+        {
+
+            if (this.ControllerContext.HttpContext.Request.Cookies.AllKeys.Contains("UserInfo"))
+            {
+                HttpCookie cookie = this.ControllerContext.HttpContext.Request.Cookies["UserInfo"];
+                cookie.Expires = DateTime.Now.AddDays(-1);
+                this.ControllerContext.HttpContext.Response.Cookies.Add(cookie);
+            }
+            Session.Clear();
+            return RedirectToAction("Login");
+        }
+        [HttpGet]
+        public ActionResult ForgetPassword()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ForgetPassword(DatabaseLayer.Customer signup)
+        {
+
+            var isEmailAlreadyExists = db.Customers.FirstOrDefault(x => x.EmailAddress == signup.EmailAddress);
+            if (isEmailAlreadyExists == null)
+            {
+                ViewBag.Message = "User Not Registerd";
+                return View();
+            }
+            else
+            {
+                SendMail(isEmailAlreadyExists.EmailAddress, isEmailAlreadyExists.Password);
+                ViewBag.Message = "Email Sent to Register Email Address";
+                return RedirectToAction("Login", "Login");
+            }
+
+        }
+        void SendMail(string toemail, string password)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("reddysowmya935@gmail.com");
+                message.To.Add(new MailAddress(toemail));
+                message.Subject = "FLight Booking System Login Password";
+                message.IsBodyHtml = true; //to make message body as html  
+                message.Body = "your Password Is : " + password;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host  
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = new NetworkCredential("reddysowmya935@gmail.com", "igfykfjfepsnrbgy");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+        public ActionResult Login([Bind(Include = "EmailAddress , Password")] DatabaseLayer.Customer model)
+        {
+            var data = db.Customers.Where(s => (s.EmailAddress.Equals(model.EmailAddress) || s.AdharCard.Equals(model.EmailAddress)) && s.Password.Equals(model.Password)).ToList();
+            if (data.Count() > 0)
+            {
+                Session["uid"] = data.FirstOrDefault().ID;
+                HttpCookie cooskie = new HttpCookie("UserInfo");
+                cooskie.Values["idUser"] = Convert.ToString(data.FirstOrDefault().ID);
+                cooskie.Values["FullName"] = Convert.ToString(data.FirstOrDefault().Name);
+                cooskie.Values["Email"] = Convert.ToString(data.FirstOrDefault().EmailAddress);
+                cooskie.Values["IsAdmin"] = Convert.ToString(data.FirstOrDefault().IsAdmin);
+                cooskie.Expires = DateTime.Now.AddMonths(1);
+                Response.Cookies.Add(cooskie);
+                if (data.FirstOrDefault().IsAdmin == "True")
+                {
+                    return RedirectToAction("Index", "Bookings");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "CustomersMaster");
+                }
+            }
+            else
+            {
+                ViewBag.Message = "Login failed";
+                return View( );
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            var userInCookie = Request.Cookies["UserInfo"];
+            if (userInCookie != null)
+            {
+                return RedirectToAction("Index", "CustomersMaster");
+            }
+            else
+            {
+                var cust = new DatabaseLayer.Customer();
+                return View(cust);
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Signup([Bind(Include = "ID,Name,EmailAddress,MobileNo,Password,IsAdmin,DOB,CIty,AdharCard,Gender")] FLightBookingSystem.DatabaseLayer.Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                if (db.Customers.Any(x => x.EmailAddress == customer.EmailAddress && x.AdharCard == x.AdharCard))
+                {
+                    ViewBag.Message = "User Already Exist";
+                    var cust1 = new DatabaseLayer.Customer();
+                    return View(cust1);
+                }
+                else
+                {
+                    customer.IsAdmin = "false";
+                    db.Customers.Add(customer);
+                    db.SaveChanges();
+                    return RedirectToAction("Login");
+                }
+            }
+            var cust = new DatabaseLayer.Customer();
+            return View(cust);
+        }
+
+    }
+}
